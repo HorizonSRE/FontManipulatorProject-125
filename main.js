c60921d45500c20bf45a1de4cd6a4a24be520d92
HorizonSRE <106289299+HorizonSRE@users.noreply.github.com>
2023-05-09 17:51:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw() {
+    background('#8B0000');
+    textSize(difference);
+    fill('#FFE787');
+    text('Surya', 50 ,400);
+}
+function modelLoaded() {
+    console.log('PoseNetis Initialized');
+}
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = leftWristX - rightWristX;
+    }
+}
